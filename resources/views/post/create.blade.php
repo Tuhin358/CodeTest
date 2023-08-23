d9340670da3104351c741b0febc8bae15090e9d9
Tuhin358 <tarifulislam2400@gmail.com>
2023-08-23 15:48:48 +0600

    up

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,7 +1,5 @@
 @extends('admin.admin_master')
 @section('admin.content')
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<meta name="csrf-token" content="{{ csrf_token() }}">
 
 <div class="row-fluid sortable">
 <div class="box span12">
@@ -66,48 +64,7 @@
 </div><!--/row-->
 </div><!--/row-->
 
-
-<script>
-     //Paginetion for page
-     $(document).on('click','.pagination a',function(e){
-        e.preventDefault();
-        let page=$(this).attr('href').split('page=')[1]
-        post(page)
-
-       })
-       function post(page){
-        $.ajax({
-            url:"/pagination/paginate-data?page="+page,
-            success:function(res){
-                $('.card-body').html(res);
-
-            }
-
-        })
-       }
-</script>
-
- //Search function
- $(document).on('keyup',function(e){
-    e.preventDefault();
-    let search_string = $('#search').val();
-    $.ajax({
-        url:"{{ route('search.post') }}",
-        method:'GET',
-        data:{search_string:search_string},
-        success:function(res){
-            $('.card-body').html(res);
-        }
-
-    });
-   })
-
-
-
-
-
-
-   @endsection
+@endsection
 
 
 
